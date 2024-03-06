58189bb140639594fb2a1314fcc72a0f82d089bb
sarah18-z <sarah.zemirli18@gmail.com>
2024-03-06 13:59:30 +0100

    Updating the address for a customer

--- P1T2.sql
+++ P1T2.sql
@@ -1,3 +1,8 @@
 -- Adding a new customer:
 INSERT INTO customers (first_name, last_name, email, city)
 VALUES ('sarah', 'zemirli', 'sarah.zemirli@example.fr', 'alger');
+
+-- Updating the address for a customer
+UPDATE customers 
+SET city = 'Montreal' 
+WHERE first_name = 'sarah' AND last_name = 'zemirli';
